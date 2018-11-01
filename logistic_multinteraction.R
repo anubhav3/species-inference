4ea8519dc5038b44643b1c0bd924174affaeda53
Owen Petchey <owen.petchey@ieu.uzh.ch>
2018-11-01 16:27:40 +0100

    many changes, working ok, but could be better

--- logistic_multinteraction.R
+++ logistic_multinteraction.R
@@ -1,148 +1,117 @@
+rm(list=ls())
+
 library(boot)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 
-#logistic model y = a*x + b  ~Food Web Model
+#set.seed(4)
+
+## This is the function that takes prey size (x) and
+## calculates the probability of a prey individual of that size
+## being eaten
+## a determines the strength of the size effect
+## b shifts the size at which the inflexion occurs
 logistic <- function(x, a, b){
-  yy <- inv.logit(a*x+b)
+  yy <- inv.logit(a*log10(x)+b)
   return(yy)
 }
-
-x <- seq(-10, 10, length=100)
-ggplot(mapping=aes(x=x, y=logistic(x, 1, 1))) +
-  geom_line()
-
 
 ## parameters
 mass_mean <- 1
 mass_sd <- 1
-num_species <- 100
-#<<<<<<< HEAD
-real_a <- 1 # ~Real Parameters
-#=======
+num_species <- 50
 real_a <- 10 # ~Real Parameters
-
 real_b <- -5 # ~Real Parameters
+num_interations_observed <- 200 ## total number of potential interactions
 
+## create species and their body sizes
+x <- rlnorm(num_species, mass_mean, mass_sd) # ~Real Body Sizes
+real_b <- real_b - mean(log10(x))
+## make a series for plotting the curve
+x_seq <- seq(min(x), max(x), length=1000)
 
-x <- log10(rlnorm(num_species, mass_mean, mass_sd)) # ~Real Body Sizes
-real_b <- real_b - mean(x)
-#The number of interactions increases with increasing body size. Species with max. body size will have 5
-#interactions and min. body size will have 1 interaction and other species in between (i.e 2,3,4).
-min_bs <- min(x)
-max_bs <- max(x)
-f <- function(x)
-{
-  f <- round(1+4*(x-min_bs)/(max_bs-min_bs))
-}
-x_data <- data.frame(bs = x, no = f(x), interactions = list(0,0,0,0,0))
+## observation process (not specified whether this is us observing the presence / absence of an interaction,
+## or if it is the predator "observing" (i.e. encountering) its prey)
+## get a number of observations of each prey
+#obs_x <- sample(x, num_interations_observed, replace=TRUE, p=x/sum(x))
+obs_x <- sample(x, num_interations_observed, replace=TRUE)
 
-# ~Real food web
-p_interaction <- logistic(x, real_a, real_b) 
 
 # ~Stochasticity in the system's biology
-interaction_org <- numeric(length=num_species)
-for(i in 1:num_species)
+# determine whether an interaction occured or not
+real_int <- numeric(length=num_interations_observed)
+for(i in 1:num_interations_observed)
   {
-  x_data[i,3:(2+f(x[i]))] <- rbinom(f(x[i]), 1, p_interaction[i])
-  interaction_org[i] <- rbinom(1, 1, p_interaction[i])
+  real_int[i] <- rbinom(1, 1, prob=logistic(obs_x[i], real_a, real_b))
   }
-  
-
-## deterministic biology version
-#interaction <- ifelse(p_interaction<0.3, 0, 1)
+#real_data <- data.frame(obs_x, real_int, p=logistic(obs_x[i], real_a, real_b))  
 
 
-## add multiple observations per interaction (perhaps body size dependent)
+# plot probability curve and the species on it
+# ggplot(mapping=aes(x=log10(x_seq), y=logistic(x_seq, real_a, real_b))) +
+#   geom_line() + 
+#   geom_point(aes(x=log10(x), y=logistic(x, real_a, real_b))) +
+#   geom_jitter(aes(x=log10(obs_x), y=real_int), col="red")
 
 
-ggplot(mapping=aes(x=x, y=p_interaction)) +
-  geom_point() + geom_line() +
-  geom_point(mapping=aes(y=interaction_org), col="red")
+## add observation error to give if we observed and interaction or not
+## p of observing x given y (p_x_y)
+p_0_1 <- 0.0  ## larger values give greater observation error
+p_1_0 <- 0.0  ## larger values give greater observation error
+rans_temp <- runif(num_interations_observed)
 
+obs_interaction <- real_int
+obs_interaction <- ifelse(real_int==1 & rans_temp<p_0_1, obs_interaction-1,
+                          ifelse(real_int==0 & rans_temp<p_1_0, obs_interaction+1, obs_interaction))
 
-## add observation error
-p_1_1 <- 0.8  ## larger values give lower observation error
-p_0_0 <- 0.7  ## larger values give lower observation error
-rans_list <- runif(num_species*5)
+ggplot(mapping=aes(x=log10(x_seq), y=logistic(x_seq, real_a, real_b))) +
+  geom_line() + 
+  geom_point(aes(x=log10(x), y=logistic(x, real_a, real_b))) +
+  geom_jitter(aes(x=log10(obs_x), y=real_int), col="red", height=0.1) +
+  geom_jitter(aes(x=log10(obs_x), y=obs_interaction), col="green", height=0.1)
 
-raw_obs_interaction <- numeric(5)
-obs_interaction <- numeric(num_species)
-count <- 1
-for(i in 1:num_species)
-  {
-  x_count <- 1
-  for(interaction in x_data[i,3:7])
-    {
-      rans <- rans_list[count]
-      raw_obs_interaction[x_count] <- ifelse(interaction==1 & rans>p_1_1, interaction-1,
-                                    ifelse(interaction==0 & rans>p_0_0, interaction+1, interaction))
-      count <- count+1
-      x_count <- x_count+1
-  }
-  print(sum(raw_obs_interaction[1:x_data[i,2]]))
-  print(x_data[i,2]/2)
-  obs_interaction[i] <- ifelse(sum(raw_obs_interaction[1:x_data[i,2]])>x_data[i,2]/2,1,0)
-  }
-
-
-
-ggplot(mapping=aes(x=x, y=p_interaction)) +
-  geom_point() + geom_line() +
-  geom_point(mapping=aes(y=interaction_org), col="red", size=3) +
-  geom_point(mapping=aes(y=obs_interaction), col="green")
 
 
 #Parameter Estimation (ABC)
-numm <- 100000
+numm <- 10000
 guesses <- data.frame(a=runif(numm,0,1000),
                       b=runif(numm,-10,10),
                       Q = rep(NA,numm))
 
 for(i in 1:numm)
 {
-  guesses$Q[i] <-  sum(1-dbinom(obs_interaction, 1, logistic(x, guesses$a[i], guesses$b[i])))
+  guesses$Q[i] <-  sum(1-dbinom(obs_interaction, 1, logistic(obs_x, guesses$a[i], guesses$b[i])))
 }
 
-## Why so many infinite values?
-
-guesses <- filter(guesses, is.finite(Q))
+sum(1-dbinom(obs_interaction, 1, logistic(obs_x, real_a, real_b)))
 
 ggplot(gather(guesses), aes(x=value)) +
   facet_wrap(~key, scales="free_x") + 
   geom_histogram()
 
-threshold <- 30
+## find threshol
+proportion_to_keep <- 0.01
+temp <- arrange(guesses, Q) %>%
+  slice(numm*proportion_to_keep)
+threshold <- temp$Q
 
 guesses %>%
-  filter(Q<threshold) %>%
+  filter(Q<=threshold) %>%
   gather() %>%
   ggplot(aes(x=value)) +
   facet_wrap(~key, scales="free_x") +
   geom_histogram()
 
-
 means <- guesses %>%
-  filter(Q<threshold) %>%
+  filter(Q<=threshold) %>%
   summarise_all(mean)
 
+ggplot(mapping=aes(x=log10(x_seq), y=logistic(x_seq, real_a, real_b))) +
+  geom_line() + 
+  geom_point(aes(x=log10(x), y=logistic(x, real_a, real_b))) +
+  geom_jitter(aes(x=log10(obs_x), y=real_int), col="red", height=0.1) +
+  geom_jitter(aes(x=log10(obs_x), y=obs_interaction), col="green", height=0.1) +
+  geom_line(aes(x=log10(x_seq), y=logistic(x_seq, means$a, means$b)), col="red", linetype="dashed")
 
-
-ggplot(mapping=aes(x=x, y=p_interaction)) +
-  geom_point() + geom_line() +
-  geom_point(mapping=aes(y=interaction_org), col="red", size=3) +
-  geom_point(mapping=aes(y=obs_interaction), col="green") +
-  geom_line(aes(y=logistic(x, means$a, means$b)), col="red", linetype="dashed")
-
-
-#<<<<<<< HEAD
-# m1 <- glm(obs_interaction ~ x, family=binomial)
-# summary(m1)
-#=======
-# m1 <- glm(obs_interaction ~ x, family=binomial)
-# summary(m1)
-
-## plot the glm curve
-## plot samples from the posteriors
 
